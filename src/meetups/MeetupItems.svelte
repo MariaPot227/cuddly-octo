a5079323d3957d1b2f86681e587a2e36a5c11dae
mariapot227 <potam2@student.op.ac.nz>
2025-11-14 12:30:59 +1300

    feat(meetups) : create component with format for each meetup display using html

---
+++ src/meetups/MeetupItems.svelte
@@ -0,0 +1,19 @@
+<script>
+    
+</script>
+
+<article>
+    <header>
+        <h1>TITLE</h1>
+        <h2>SUBTITLE</h2>
+    </header>
+    <div class="image">
+        <img src="" alt="">
+    </div>
+    <div class="content">
+        <p></p>
+    </div>
+    <footer>
+        
+    </footer>
+</article>
