3a10f0f61c9c8df94c80446c65554cb63684f4e1
mariapot227 <potam2@student.op.ac.nz>
2025-11-14 12:30:25 +1300

    feat(meetup) : implement hard code meetup information in an array

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -495,13 +495,35 @@ var app = (function () {
     function instance($$self, $$props, $$invalidate) {
     	let { $$slots: slots = {}, $$scope } = $$props;
     	validate_slots('App', slots, []);
+
+    	const meetups = [
+    		{
+    			id: 'm1',
+    			title: 'Coding Bootcamp',
+    			subtitle: 'Learn to code in 2 hours',
+    			description: 'In meetup, we will have experts teach you how to code',
+    			imageUrl: 'https://images.squarespace-cdn.com/content/v1/5fa1522044bdda192713063c/1d041c11-e762-4641-993b-591c9de72b76/Coffee+Beer+Cafe+in+Tiaong%2C+Quezon',
+    			address: '27th New Road, 23562 New York',
+    			contactEmail: 'code@test.com'
+    		},
+    		{
+    			id: 'm2',
+    			title: 'Swim Together',
+    			subtitle: 'Swimming lessons',
+    			description: 'In meetup, we will have experts teach you how to swim',
+    			imageUrl: 'https://kidsonboard.co.nz/wp-content/uploads/2016/07/Moana-Pool-440x230.jpg',
+    			address: '27th Moana Pool, 11111 New York',
+    			contactEmail: 'swim@test.com'
+    		}
+    	];
+
     	const writable_props = [];
 
     	Object.keys($$props).forEach(key => {
     		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== '$$' && key !== 'slot') console.warn(`<App> was created with unknown prop '${key}'`);
     	});
 
-    	$$self.$capture_state = () => ({ Header });
+    	$$self.$capture_state = () => ({ Header, meetups });
     	return [];
     }
 
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,31 @@
 <script>
     import Header from "./UI/Header.svelte";
+
+    const meetups = [
+        {
+            id: 'm1',
+            title: 'Coding Bootcamp',
+            subtitle: 'Learn to code in 2 hours',
+            description: 'In meetup, we will have experts teach you how to code',
+            imageUrl: 'https://images.squarespace-cdn.com/content/v1/5fa1522044bdda192713063c/1d041c11-e762-4641-993b-591c9de72b76/Coffee+Beer+Cafe+in+Tiaong%2C+Quezon',
+            address: '27th New Road, 23562 New York',
+            contactEmail: 'code@test.com'
+        },
+        {
+            id: 'm2',
+            title: 'Swim Together',
+            subtitle: 'Swimming lessons',
+            description: 'In meetup, we will have experts teach you how to swim',
+            imageUrl: 'https://kidsonboard.co.nz/wp-content/uploads/2016/07/Moana-Pool-440x230.jpg',
+            address: '27th Moana Pool, 11111 New York',
+            contactEmail: 'swim@test.com'
+        }
+    ];
 </script>
+
+<Header />
 
 <style>
     
 </style>
-<Header />
-
 
